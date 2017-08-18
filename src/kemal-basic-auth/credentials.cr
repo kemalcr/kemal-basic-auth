cafe8a00d94c0d390579c92c9fa9cc9a78cecd16
Will Leinweber <will@bitfission.com>
2017-08-18 08:52:44 -0700

    Prevent timing attacks

--- src/kemal-basic-auth/credentials.cr
+++ src/kemal-basic-auth/credentials.cr
@@ -1,14 +1,31 @@
+require "crypto/subtle"
+
 class HTTPBasicAuth
   class Credentials
     def initialize(@entries : Hash(String, String) = Hash(String, String).new)
     end
 
-    def authorize?(username : String, password : String) : String?
-      if @entries[username]? == password
+    def authorize?(username : String, given_password : String) : String?
+      test_password = find_password(username, given_password)
+      if Crypto::Subtle.constant_time_compare(test_password, given_password)
         username
       else
         nil
       end
+    end
+
+    private def find_password(username, given_password)
+      # return a password that cannot possibly be correct if the username is wrong
+      pw = "not #{given_password}"
+
+      # iterate through each possibility to not leak info about valid usernames
+      @entries.each do |(user, password)|
+        if Crypto::Subtle.constant_time_compare(user, username)
+          pw = password
+        end
+      end
+
+      pw
     end
   end
 end
